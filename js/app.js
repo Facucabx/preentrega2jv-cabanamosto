411fb77751532698dab3b4356d3f13432f0c078a
Your Name <you@example.com>
2023-08-24 19:04:44 -0300

    Se aplica almacenar datos en el archivo JSON, cargarlo de forma asíncrona utilizando fetch y async-await

--- js/app.js
+++ js/app.js
@@ -1,60 +1,18 @@
-//Aca se carga la BD desde el Localstorage
-const storedDatabase = localStorage.getItem("database");
-const database = storedDatabase ? JSON.parse(storedDatabase) : [
-    {
-        nombre: "Juan",
-        servicio: "cerrajero",
-        telefono: "555-1234",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Apertura de puertas", "Cambio de cerraduras"]
-    },
-    {
-        nombre: "Esteban",
-        servicio: "cerrajero",
-        telefono: "555-1234",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Apertura de puertas", "Cambio de cerraduras"]
-    },
-    {
-        nombre: "María",
-        servicio: "electricista",
-        telefono: "555-5678",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Instalación eléctrica", "Reparación de cortocircuitos"]
-    },
-    {
-        nombre: "Carlos",
-        servicio: "fletes",
-        telefono: "555-9876",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Mudanzas locales", "Transporte de carga"]
-    },
-    {
-        nombre: "Laura",
-        servicio: "gasista",
-        telefono: "555-4321",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Reparación de gas", "Instalación de tuberías"]
-    },
-    {
-        nombre: "Pedro",
-        servicio: "veterinario",
-        telefono: "555-7890",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Consultas médicas", "Vacunación de mascotas"]
-    },
-    {
-        nombre: "Ana",
-        servicio: "tecnico",
-        telefono: "555-5555",
-        ubicacion: "San Nicolás, Buenos Aires, Argentina",
-        serviciosOfrecidos: ["Reparación de electrodomésticos", "Mantenimiento preventivo"]
+// Función para cargar profesionales desde un archivo JSON
+async function cargarProfesionalesDesdeJSON() {
+    try {
+        const response = await fetch("./profesionales.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al cargar datos desde el archivo JSON", error);
+        return [];
     }
-];
+}
 
-// Función para buscar los profesionales
-function buscarProfesionales(servicio) {
-    return database.filter(profesional => profesional.servicio === servicio);
+// Función para buscar los profesionales en el archivo JSON
+function buscarProfesionalesEnJSON(servicio, data) {
+    return data.filter(profesional => profesional.servicio === servicio);
 }
 
 // Función para mostrar resultados
@@ -72,12 +30,12 @@ function mostrarResultados(resultados) {
             const serviciosOfrecidos = profesional.serviciosOfrecidos.map(servicio => `<li>${servicio}</li>`).join("");
 
             profesionalDiv.innerHTML = `
-          <h3>${profesional.nombre}</h3>
-          <p><strong>Teléfono:</strong> ${profesional.telefono}</p>
-          <p><strong>Ubicación:</strong> ${profesional.ubicacion}</p>
-          <p><strong>Servicios ofrecidos:</strong></p>
-          <ul>${serviciosOfrecidos}</ul>
-        `;
+                <h3>${profesional.nombre}</h3>
+                <p><strong>Teléfono:</strong> ${profesional.telefono}</p>
+                <p><strong>Ubicación:</strong> ${profesional.ubicacion}</p>
+                <p><strong>Servicios ofrecidos:</strong></p>
+                <ul>${serviciosOfrecidos}</ul>
+            `;
 
             resultadosDiv.appendChild(profesionalDiv);
         });
@@ -86,14 +44,16 @@ function mostrarResultados(resultados) {
 
 // Capturar evento de clic en el botón
 const buscarBtn = document.getElementById("buscarBtn");
-buscarBtn.addEventListener("click", () => {
+buscarBtn.addEventListener("click", async () => {
     const servicioSelect = document.getElementById("servicio");
     const servicioBuscado = servicioSelect.value;
-    const resultadosBusqueda = buscarProfesionales(servicioBuscado);
+    
+    // Cargar los profesionales desde el archivo JSON
+    const data = await cargarProfesionalesDesdeJSON();
+    
+    // Buscar los profesionales en los datos cargados desde el JSON
+    const resultadosBusqueda = buscarProfesionalesEnJSON(servicioBuscado, data);
+    
+    // Mostrar los resultados
     mostrarResultados(resultadosBusqueda);
-});
-
-// Guardar la BD actualizada en el Localstorage
-window.addEventListener("beforeunload", () => {
-    localStorage.setItem("database", JSON.stringify(database));
 });
